47a9d067b1b1d4e8bfee9a5295202ec258544b26
lukyamuzi gabriella <g.bayinze004@gmail.com>
2024-10-28 21:43:09 +0300

    Update quickNotes.py

--- quickNotes.py
+++ quickNotes.py
@@ -11,4 +11,4 @@ courseUnit6 = "Writing and Study Skills"
 month1 = "May"
 month = "September"
 
-print(f"I joined the university in the year {year} doing {course} which commences for {duration} years. I started my first semester in {month1} which was a bit difficult but God led me through. In the first semester, I was doing six course units which included;{courseUnit1},{courseUnit2},{courseUnit3},{courseUnit4},{courseUnit5} and {courseUnit6}.")
+print(f"I joined the university in the year {year} doing {course} which commences for {duration} years. I started my first semester in {month1} which was a bit difficult but God led me through. In the first semester, I was doing six course units which included;{courseUnit1},{courseUnit2},{courseUnit3},{courseUnit4},{courseUnit5} and {courseUnit6}. The last two course units {courseUnit5} and {courseUnit6} are privately for our University only.")
