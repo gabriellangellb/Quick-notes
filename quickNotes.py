59efc0ea2b73e14fa27f01a44dcc6e4d4689893b
lukyamuzi gabriella <g.bayinze004@gmail.com>
2024-10-28 21:29:47 +0300

    Create quickNotes.py

---
+++ quickNotes.py
@@ -0,0 +1,14 @@
+#Brief Description of My First Year of University
+year = 2024
+duration = 3
+course = "Bachelor of Science in Information Technology"
+courseUnit1 = "Essentials of Computer Hardware and Software"
+courseUnit2 = "Fundamentals of Computing"
+courseUnit3 = "Information Systems"
+courseUnit4 = "Structured Programming"
+courseUnit5 = "Understanding the Old Testament"
+courseUnit6 = "Writing and Study Skills"
+month1 = "May"
+month = "September"
+
+print(f"I joined the university in the year {year} doing {course} which commences for {duration} years. I started my first semester in {month1} which was a bit difficult but God led me through. In the first semester, I was doing six course units which included;{courseUnit1},{courseUnit2},{courseUnit3},{courseUnit4},{courseUnit5} and {courseUnit6}.")
